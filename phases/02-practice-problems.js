0a92b2508db11c64615d0c2701966bc3b1cab787
AllenCook <albcook1313@hotmail.com>
2024-05-17 14:07:49 -0700

    update repo

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,25 +1,59 @@
 function anagrams(str1, str2) {
-  // Your code here 
+  for(let i = 0; i < str1.length; i++) {
+    if(!str1.includes(str2[i])) return false;
+  }
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here 
+  let res = [];
+  let small;
+  let big;
+  if(arr1.length < arr2.length) {
+    small = arr1;
+    big = arr2
+  } else {
+    small = arr2;
+    big = arr1;
+  }
+  for(let i = 0; i < small.length; i++) {
+    if(big.includes(small[i])) {
+      res.push(small[i]);
+    }
+  }
+  return res;
+  
+   
 }
 
 
 function duplicate(arr) {
-  // Your code here 
+  let counter = {};
+  for(let num of arr) {
+    if(counter[num] === undefined) {
+      counter[num] = 1;
+    } else {
+      return num;
+    }
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here 
+  const obj = {};
+  for (const num of nums) {
+      if (obj[target - num]) {
+        return true;
+      }
+      obj[num] = true;
+  }
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
-  // Your code here 
+   
 }
 
 
